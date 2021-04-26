ae7ccb9189ca0a23c294ee79c82259671e131fea
__gyulka__ <romagyuli-zade@yandex.ru>
2021-04-26 22:25:34 +0500

    weather

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import discord
+from discord.ext import commands
 import requests
 from io import BytesIO
 import asyncio
@@ -9,56 +9,116 @@ class Arg_err(Exception):
     pass
 
 
-users = 0
-bots = 0
-bot = discord.Client()
-emoji = ['🛁', '🚴', '🚀', '🚁', '🚂', '🚃', '🚌', '🚎', '🚑', '🚒', '🚓', '🦆', '🚕', '🦚', '🦞', '🚗',
-         '🦑', '🚚', '🦢', '🦟', '🦠', '🦅', '🦀', '🦗', '🦋', '🚜', '🦇', '🦔', '🦓', '🚣', '🦒', '🦎',
-         '🚶', '🛌', '🛒', '🛩', '🛰', '🛸', '🤔', '🤐', '🤓', '🤡', '🤫', '🥐', '🥕', '🥝', '🥦', '🥾']
+headers = {"X-Yandex-API-Key": '40d1649f-0493-4b70-98ba-98533de7710b'}
 
 
-@bot.event
-async def on_message(self, message):
-    global users, bots
-    random.shuffle(emoji)
-    if message.author == self.user:
-        return
-    if message.content == '/start' or message.content == '/help' :
-        await message.channel.send(f'играйте же в игру"')
-    elif message.content == '/stop':
-        users = 0
-        bots = 0
-        await message.channel.send('Buy')
+def get_coords(toponym_to_find):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": toponym_to_find,
+        "format": "json"}
+
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+
+    if response:
+        json_response = response.json()
+        toponym = json_response["response"]["GeoObjectCollection"][
+            "featureMember"][0]["GeoObject"]
+        toponym_coodrinates = toponym["Point"]["pos"]
+        return toponym_coodrinates.split(" ")
     else:
-        try:
-            if emoji:
-                card = int(message.content)
-                user_turn = emoji.pop(card % len(emoji))
-                bot_turn = emoji.pop(random.randint(0, 100) % len(emoji))
-                if user_turn > bot_turn:
-                    users += 1
-                else:
-                    bots += 1
-                await message.channel.send(f'Your emoji {user_turn}\nBot emoji {bot_turn}\n'
-                                           f'Score: You {users} - Bot {bots}')
-            else:
-                raise IndexError
-        except IndexError:
-            if users > bots:
-                await message.channel.send(f'Emoticons are over\nScore: You {users} - Bot {bots}\n'
-                                           f'You win!')
-            elif users < bots:
-                await message.channel.send(f'Emoticons are over\nScore: You {users} - Bot {bots}\n'
-                                           f'Bot win!')
-            else:
-                await message.channel.send(f'Emoticons are over\nScore: You {users} - Bot {bots}\n'
-                                           f'Draw result!')
-        except Exception as error:
-            print(error.__str__(), message.content)
-            await message.channel.send('error')
+        return None, None
+
+
+def weather_response(place):
+    coords = get_coords(place)
+    weather_api_server = 'https://api.weather.yandex.ru/v1/forecast?'
+    weather_params = {
+        "lon": float(coords[0]),
+        "lat": float(coords[1]),
+        "lang": "ru_RU"
+    }
+    response = requests.get(weather_api_server, weather_params, headers=headers)
+    return response.json()
+
+
+def current_weather(response):
+    city = response["info"]["tzinfo"]["name"].split('/')[-1]
+    date = response["now_dt"][:10]
+    offset = response["info"]["tzinfo"]["offset"] // 3600
+    time = response["now_dt"][11:16]
+    h, m = map(int, time.split(':'))
+    time = f'{h + offset}:{m:02}'
+    fact = response["fact"]
+    temp = fact["temp"]
+    condition = fact["condition"]
+    wind_dir = fact["wind_dir"]
+    wind_speed = fact["wind_speed"]
+    pressure = fact["pressure_mm"]
+    humidity = fact["humidity"]
+    return f'Current weather in {city} today {date} at time {time}:\n' \
+           f'Temperature: {temp},\n' \
+           f'Pressure: {pressure} mm,\n' \
+           f'Humidity: {humidity}%,\n' \
+           f'{condition},\n' \
+           f'Wind {wind_dir}, {wind_speed} m/s.'
+
+
+def forecast_weather(response, days):
+    city = response["info"]["tzinfo"]["name"].split('/')[-1]
+    forecasts = response["forecasts"]
+    result = []
+    for forecast in forecasts[1:days + 1]:
+        date = forecast["date"]
+        temp = forecast["parts"]["day"]["temp_avg"]
+        condition = forecast["parts"]["day"]["condition"]
+        wind_dir = forecast["parts"]["day"]["wind_dir"]
+        wind_speed = forecast["parts"]["day"]["wind_speed"]
+        pressure = forecast["parts"]["day"]["pressure_mm"]
+        humidity = forecast["parts"]["day"]["humidity"]
+        result.append(f'Weather forecast in {city} for {date}:\n'
+                      f'Temperature: {temp},\n'
+                      f'Pressure: {pressure} mm,\n'
+                      f'Humidity: {humidity}%,\n'
+                      f'{condition},\n'
+                      f'Wind {wind_dir}, {wind_speed} m/s.')
+    return '\n\n'.join(result)
+
+
+class Bot(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.place = 'Саратов'
+
+    @commands.command(name='help_bot')
+    async def help(self, ctx):
+        message = 'Commands:\n"$!place" for changing forecast place\n' \
+                  '"$!current" for current weather message\n' \
+                  '"$!forecast _" for int days prediction'
+        await ctx.send(message)
+
+    @commands.command(name='place')
+    async def place(self, ctx, city):
+        self.place = city
+        message = f'Place changed to {self.place}'
+        await ctx.send(message)
+
+    @commands.command(name='current')
+    async def current(self, ctx):
+        response = weather_response(self.place)
+        message = current_weather(response)
+        await ctx.send(message)
+
+    @commands.command(name='forecast')
+    async def forecast_days(self, ctx, days):
+        message = forecast_weather(weather_response(self.place), int(days))
+        await ctx.send(message)
 
 
 TOKEN = 'ODM1OTgzMjY0NzQwNTQwNDY2.YIXYFg.pbBz6d-ZoRWTinFGN_Xrk69LH1g'
 
-
+bot = commands.Bot(command_prefix='#!')
+bot.add_cog(Bot(bot))
 bot.run(TOKEN)
